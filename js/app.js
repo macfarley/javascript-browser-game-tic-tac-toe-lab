a4d081fc0a5ebb631df5e0d5465682b92cc509ce
macfarley90 <therealmccoyster@gmail.com>
2025-02-13 16:30:45 -0500

    started from the beginning

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+
 /*-------------------------------- Constants --------------------------------*/
+//5) Define the required constants.
 const winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -9,163 +11,122 @@ const winningCombos = [
     [0, 4, 8],
     [2, 4, 6]
   ]
-//player X's squares
-const playerX = []
-const playerO = []
-//player O's squares
-
-//3) Upon loading, the game state should be initialized, and a function should 
-//   be called to render this game state.
-
-//4) The state of the game should be rendered to the user.
-
-//5) Define the required constants.
-
-//6) Handle a player clicking a square with a `handleClick` function.
-
-//7) Create Reset functionality.
-
+  
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 //1) Define the required variables used to track the state of the game.
 
-let board = ['', '', '', '', '', '', '', '', '']
-let turn = 'O'
-let winner = true
-let tie = false
+let board = ['1', '2', '3', '4', '5', '6', '7', '8', 'X']; 
+let turn = 'X';
+let winner = false;
+let tie = false;
 
 
 /*------------------------ Cached Element References ------------------------*/
 //2) Store cached element references.
-const squareEls = document.querySelectorAll('.sqr')
-const messageEl = document.querySelector('#message')
 
 
+//constant targets an array of the square divs
+const squareEls = document.querySelectorAll('.sqr');
+//targets a header visible to user
+const messageEL = document.querySelector('#message');
+//target reset button
+const resetButtonEl = document.querySelector('#reset')
 
 
 /*-------------------------------- Functions --------------------------------*/
-function placePiece(event){
-    // when i click, i want it to spit out which square i clicked
-    const squareIndex = event.target.id
-    console.log(squareIndex)
-    //check the array at the index you clicked
-    if(board[squareIndex] != ''){
-        return
-    } else {
-     board[squareIndex] = turn
-     console.log(board[squareIndex])
-     updateBoard()
-     if(turn == 'X'){ 
-        playerX.push(squareIndex)
-        }else if(turn == 'O'){
-        playerO.push(squareIndex)
-        }
-    }
-}
-
-function checkForWinner(){
-    //look through the winningcombos array
-    for(let i=0; i < winningCombos.length; i++){
-        //then through the arrays inside
-        for(let j=0; j < winningCombos[i].length; j++){
-        //if the j has something in the playerX array
-        if(winningCombos[i][j].includes(playerX))
-        winner = true
-        console.log("X wins")}
-    }
-    
-    if(winningCombos.includes(playerO.sort())){
-        winner = true
-        console.log("O wins")
-    }
-    else{
-        winner = false
-        console.log("keep playing")
-    }
-}
-
-// function switchPlayers(){
-//     if(turn == 'X'){
-//         turn = 'O'
-//     } else{
-//         turn = 'X'
-//     }
-// }
-
-function handleClick(event){
-    console.log('clicked here', event)
-    // // when i click, i want it to spit out which square i clicked
-    // const squareIndex = event.target.id
-    // console.log(squareIndex)
-    // //check the array at the index you clicked
-    // if(board[squareIndex] != ''){
-    //     return
-    // } else {
-    //  board[squareIndex] = turn
-    //  console.log(board[squareIndex])
-    //  updateBoard()
-    //  if(turn == 'X'){ 
-    //     playerX.push(squareIndex)
-    //     }else if(turn == 'O'){
-    //     playerO.push(squareIndex)
-    //     }
-    // }
-    placePiece()
-    
-    console.log(playerX, playerO)
-    //then check the current player's array vs the winning combos to spit out boolean for winner
-    checkForWinner()
-    if(winner == false && tie == false){
-        // switchPlayers()
-    }
+//call this when it's time to update the user about game state
+function render(){
+    updateBoard()
     updateMessage()
 }
 
-
-
-function render(){
-
-
-}
-function updateBoard(){
-    console.log(board)
-    //go through the squares, and put a letter in each one
-    board.forEach((element, index) => {
-        if(element == 'X'){
-        squareEls[index].innerHTML = 'X'}
-        else if(element == 'O'){
-            squareEls[index].innerHTML = 'O'}
-        else {squareEls[index].innerHTML = ''}
-    //id of the div square a player chooses
-    });
-    console.log(board)}
-updateBoard()
-
-function updateMessage(){
-    if(winner == false && tie == false){
-        messageEl.innerText = `It's ${turn}'s turn now.`
-    }else if(tie == true && winner == false) {
-        messageEl.innerText = `tie game.`
-    } else { messageEl.innerText = ` is the winner.`}
-}
-function firstPlayer(){
-    const randomIndex = Math.floor(Math.random())
-    const player = ['X', 'O']
-    turn = player[randomIndex]
-}
-
+//initializes game, runs itself on page landing
 function init(){
-    firstPlayer()
     render()
+    console.log('game loaded')
+};
+
+//where we actually store the state of the game
+function updateBoard(){
+    board.forEach(function(square, index){
+    squareEls[index].innerHTML = square
+    // console.log(`Square ${index+  1} is ${square}`)
+})}
+
+//targets the header with the state of game information to users
+//4) The state of the game should be rendered to the user.
+function updateMessage(){
+    //check if game isnt over
+    if(winner == false && tie == false){
+        messageEL.innerHTML = `It's ${turn}'s turn.`
+    }
+    if(winner == false && tie == true){
+        messageEL.innerHTML = `It's a tie game.`
+    }
+    if(winner == true && tie == false){
+        messageEL.innerHTML = `${turn} is the winner.  Rematch?`
+    }
+
+}
+
+// change the index of the board to the square was clicked
+function placePiece(index){
+    board[index] = turn
+    console.log(board)
+}
+
+//compare the selections of each player, to winning combos
+// function checkForWinner(){
+    for(let i=0; i < winningCombos.length; i++){
+      if(board[i] != ''){
+        if(winningCombos[i])
+      }
+    }
+
+
+// all the squares are full and winner is false
+function checkForTie(){
+
+}
+
+//no winner and no tie means keep playing, make turn the opposite player
+function switchPlayerTurn(){
+
+}
+
+
+//6) Handle a player clicking a square with a `handleClick` function.
+function handleClick(event){
+    const squareIndex = event.target.id
+    //check if the game is already over.
+    // if(board[squareIndex] == 'X'||'O'){
+    //     return "taken"
+    // }
+    // if(winner == true){
+    //     return "game is over"
+    // }
+    placePiece(squareIndex)
+    // render()
+//     checkForWinner()
+//     console.log('winner?')
+//     checkForTie()
+//     console.log('tie game?')
+//     switchPlayerTurn()
+//     console.log('next!')
 }
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-document.addEventListener('DOMContentLoaded', function() {
-    init()
-    console.log("Let's get it on!!");
-  });
+//3) Upon loading, the game state should be initialized, and a function should 
+//   be called to render this game state.
+document.addEventListener("DOMContentLoaded",init());
 
+//when the player clicks any square, run the handleclick function when clicked
 for(let i=0; i < squareEls.length; i++){
-squareEls[i].addEventListener('click', handleClick)}
+    squareEls[i].addEventListener('click', handleClick)
+}
+
+//7) Create Reset functionality.
+// resetButtonEl.addEventListener('click', init());
