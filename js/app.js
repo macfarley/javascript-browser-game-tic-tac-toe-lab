4f5cb69fde4503b6f1d793e0352bf2371ba0d738
macfarley90 <therealmccoyster@gmail.com>
2025-02-16 13:42:09 -0500

    fully functional

--- js/app.js
+++ js/app.js
@@ -11,28 +11,22 @@ const winningCombos = [
     [0, 4, 8],
     [2, 4, 6]
   ]
-  
-
-
-/*---------------------------- Variables (state) ----------------------------*/
+  /*---------------------------- Variables (state) ----------------------------*/
 //1) Define the required variables used to track the state of the game.
-
 let board = ['', '', '', '', '', '', '', '', '']; 
-let turn = 'X';
-let winner = false;
-let tie = false;
-
-
+let turn = 'X'
+let winner = false
+let tie = false
 /*------------------------ Cached Element References ------------------------*/
 //2) Store cached element references.
-
-
 //constant targets an array of the square divs
 const squareEls = document.querySelectorAll('.sqr');
 //targets a header visible to user
 const messageEL = document.querySelector('#message');
 //target reset button
 const resetButtonEl = document.querySelector('#reset')
+//a header with the current turn display
+const currentTurnEl = document.querySelector('#turnCounter')
 
 
 /*-------------------------------- Functions --------------------------------*/
@@ -47,25 +41,20 @@ function updateBoard(){
 //4) The state of the game should be rendered to the user.
 function updateMessage(){
     //check if game isnt over
-    if(winner == false && tie == false){
-        messageEL.innerHTML = `It's ${turn}'s turn.`
-    }
     if(winner == false && tie == true){
-        messageEL.innerHTML = `It's a tie game.`
-    }
-    if(tie == false && winner == true){
-        messageEL.innerHTML = `${turn} is the winner.  Rematch?`
-    }
-
+        messageEL.innerHTML = `It's a draw.`
+        }else if(tie == false && winner == true){
+        messageEL.innerHTML = `${turn} is the winner.`
+        }else{
+             messageEL.innerHTML = `Keep playing.`
+        }
 }
-
 
 //call this when it's time to update the user about game state
 function render(){
     updateBoard()
     updateMessage()
-        // console.log(winner, tie, board)
-
+    // console.log(winner, tie, board)
 }
 
 //initializes game, runs itself on page landing
@@ -75,11 +64,12 @@ function init(){
     for(let i = 0; i < board.length; i++){
         board[i] = ''
     }
-    render()
+    updateBoard()
+    turn = 'X'
+    messageEL.innerHTML = `New Game.`
+    currentTurnEl.innerHTML = `It's ${turn}'s turn.`
     console.log('game loaded')
 };
-
-
 
 // change the index of the board to the square was clicked
 function placePiece(index){
@@ -89,80 +79,75 @@ function placePiece(index){
 
 //compare the selections of each player, to winning combos
 function checkForWinner(){
+    // console.log(turn, board)
     //go to each combo
     for(let i = 0; i < winningCombos.length; i++){
-        // //then for each index of that combo, see if the value of the board at that index is the current turn player, 
-        // // only the current player can win
-        // for(let j = 0; j < winningCombos[i].length; j++){
-        //     if(board[j] == turn){
-        //         //all three of a winning combo's squares are the same turn, that one wins
-        //         if(board[winningCombos[i][0]] == turn && board[winningCombos[i][1]] == turn && board[winningCombos[i][2]] == turn){
-        //             winner = true
-        //             console.log(`winner is ${turn}`)
-        //         }
-        //     }
-        // }
     //find the value of winning combos 0index
     //exclude the empty strings
-        if(board[winningCombos[i][0]] != ""){
-             //if the vlue of these squares isn't the same, they're not owned by the same player
-            if(board[winningCombos[i][0]] == board[winningCombos[i][1]]){
+        if(board[winningCombos[i][0]] == ""){
+            continue}
+        //if the vlue of these squares isn't the same, they're not owned by the same player
+        if(board[winningCombos[i][0]] == board[winningCombos[i][1]] && board[winningCombos[i][1]] != ''){
                 if(board[winningCombos[i][0]] == board[winningCombos[i][2]]){
+                    console.log("winner!")
                     winner = true
-                    // console.log("winner!")
+                    return
                 }
-                else return
+                else continue
             } 
-            else return       
-        } 
+        else continue       
+        }
+    //if it makes through the whole loop with no trigger
+    console.log("no winning combo")
     }
-}
 
 // all the squares are full and winner is false
 function checkForTie(){
     //if there's a winner there's no tie
-    if(winner = true){
+    if(winner == true){
         return
-    }
+    }else{
     //if there's unused spots it can't be a tie yet
     if(board.indexOf('') < 0){
         tie = true    
-    }
-    console.log('tie game losers')
+    }}
+    // console.log('tie game losers')
 }
 
 //no winner and no tie means keep playing, make turn the opposite player
-function switchPlayerTurn(){
-    if(winner != true){
-        if(turn == 'X'){
-            turn = 'O'
-        }
-        if(turn == 'O'){
-            turn = 'X'
-        }
-        console.log(turn)
+function switchPlayerTurn() {
+    if(winner == true||tie == true){
+        currentTurnEl.innerHTML = `Rematch?`
+        return
+    }else{
+    if(turn == 'X'){
+        turn = 'O'
+    }else{
+    if(turn == 'O'){
+        turn = 'X'
+    }}
+    currentTurnEl.innerHTML = `It's ${turn}'s turn.`
     }
-    return
-}
+    // console.log(turn)
+  };
 
 
 //6) Handle a player clicking a square with a `handleClick` function.
 function handleClick(event){
     const squareIndex = event.target.id
-    // // check if the game is already over.
-    // if(board[squareIndex] == 'X'||'O'){
-    //     return "taken"
-    // }
-    // if(winner == true){
-    //     return "game is over"
-    // }
+    // check if the game is already over.
+    if(winner === true){
+        console.log("the game is over")
+        return}
+    if(board[squareIndex] == 'X' || board[squareIndex] == 'O'){
+        console.log("Square taken.")
+        return
+    }
     placePiece(squareIndex)
     checkForWinner()
     checkForTie()
-    switchPlayerTurn()
     render()
-
-//     console.log('next!')
+    switchPlayerTurn()
 }
 
 
